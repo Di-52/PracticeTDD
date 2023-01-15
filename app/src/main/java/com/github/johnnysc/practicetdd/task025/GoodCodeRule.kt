86d810e580185c6add95b47ad85474a4196284c6
Di.Est <di.est.52@gmail.com>
2023-01-15 22:17:44 +0300

    solution for task-025 by regular expression

---
+++ app/src/main/java/com/github/johnnysc/practicetdd/task025/GoodCodeRule.kt
@@ -0,0 +1,15 @@
+package com.github.johnnysc.practicetdd.task025
+
+/**
+ * @author Demitrist on 15.01.2023
+ **/
+
+interface GoodCodeRule {
+
+    fun isValid(text: String):Boolean
+
+    class Incapsulation : GoodCodeRule {
+        override fun isValid(text: String): Boolean =
+            !text.contains(Regex("""(?<!(protected|private)\s)(val|var)"""))
+    }
+}
