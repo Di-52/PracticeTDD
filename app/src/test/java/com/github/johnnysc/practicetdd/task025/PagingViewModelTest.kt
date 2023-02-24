1c2a0ad11e7324651ac90c17e022519a92114b2a
Di.Est <di.est.52@gmail.com>
2023-02-24 21:49:43 +0300

    tests move to folder

--- app/src/test/java/com/github/johnnysc/practicetdd/task025/PagingViewModelTest.kt
+++ app/src/test/java/com/github/johnnysc/practicetdd/task025/PagingViewModelTest.kt
@@ -2,6 +2,7 @@ package com.github.johnnysc.practicetdd
 
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.Observer
+import com.github.johnnysc.practicetdd.task025.*
 import org.junit.Assert.assertEquals
 import org.junit.Test
 
